d8cb1edb845c1e43c2270e16125a7300c8e2aa77
sanjana awachat <you@example.com>
2024-12-08 15:05:51 +0530

    machinetest

---
+++ src/app/table/table.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  stuarr=[{
+    fname:"sanjana",
+    lname:"awachat",
+    email:"san@gmail.com",
+    contact:1234456789
+  },
+  {
+    fname:"sanjana",
+    lname:"awachat",
+    email:"san@gmail.com",
+    contact:1234456789
+  },
+  {
+    fname:"sanjana",
+    lname:"awachat",
+    email:"san@gmail.com",
+    contact:1234456789
+  },
+  {
+    fname:"sanjana",
+    lname:"awachat",
+    email:"san@gmail.com",
+    contact:1234456789
+  },
+  {
+    fname:"sanjana",
+    lname:"awachat",
+    email:"san@gmail.com",
+    contact:1234456789
+  },
+  {
+    fname:"sanjana",
+    lname:"awachat",
+    email:"san@gmail.com",
+    contact:1234456789
+  }]
+}
